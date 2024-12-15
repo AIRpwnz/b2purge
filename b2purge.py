54e41d611075830faa9d3937b612e3ba00315a44
AIRpwnz <air@uhost.ovh>
2024-12-15 17:05:51 +0200

    Initial commit

---
+++ b2purge.py
@@ -0,0 +1,40 @@
+import argparse
+import b2sdk.v2 as b2
+import os
+from datetime import datetime, timedelta
+
+def delete_old_files(bucket_name, folder_path, days, dry_run):
+    info = b2.InMemoryAccountInfo()
+    b2_api = b2.B2Api(info)
+    application_key_id = os.getenv('B2_APPLICATION_KEY_ID')
+    application_key = os.getenv('B2_APPLICATION_KEY')
+    b2_api.authorize_account("production", application_key_id, application_key)
+
+    bucket = b2_api.get_bucket_by_name(bucket_name)
+    folder_path = folder_path.rstrip('/') + '/'
+
+    cutoff_date = datetime.now() - timedelta(days=days)
+
+    for file_version, _ in bucket.ls(folder_path, recursive=True):
+        file_mod_time = datetime.fromtimestamp(file_version.upload_timestamp / 1000)
+
+        if file_mod_time < cutoff_date:
+            if dry_run:
+                print(f"Dry run: Would delete {file_version.file_name} (last modified: {file_mod_time})")
+            else:
+                print(f"Deleting {file_version.file_name} (last modified: {file_mod_time})")
+                bucket.delete_file_version(file_version.id_, file_version.file_name)
+
+def main():
+    parser = argparse.ArgumentParser(description='Delete old files from B2 bucket.', allow_abbrev=False)
+    parser.add_argument('bucket_name', type=str, help='Name of the B2 bucket')
+    parser.add_argument('folder_path', type=str, help='Path to the folder in the B2 bucket')
+    parser.add_argument('days', type=int, help='Number of days old the files should be to be deleted')
+    parser.add_argument('--dry-run', action='store_true', help='Perform a dry run without deleting files')
+
+    args = parser.parse_args()
+
+    delete_old_files(args.bucket_name, args.folder_path, args.days, args.dry_run)
+
+if __name__ == '__main__':
+    main()
